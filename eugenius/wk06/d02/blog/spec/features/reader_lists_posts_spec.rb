f979389d18a615684cb53b584b7905e208f4a643
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2014-12-16 12:48:08 +1100

    added scenario for one blog post

--- eugenius/wk06/d02/blog/spec/features/reader_lists_posts_spec.rb
+++ eugenius/wk06/d02/blog/spec/features/reader_lists_posts_spec.rb
@@ -3,5 +3,20 @@ require 'rails_helper'
 feature "ReaderListsPosts", :type => :feature do
 	scenario "there is no blog posts" do
 		visit '/'
+		expect(page).to have_content("No blog posts found")
+	end
+
+	scenario "one blog post" do
+		Post.create(
+			title: "Hello World",
+			author: "Jane Burken"
+			content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+								Labore reprehenderit doloribus hic itaque cupiditate unde est doloremque." 
+		)
+		visit '/'
+		expect(page).to have_content("Hello World")
+		expect(page).to have_content("Jane Burken")
+		expect(page).to have_content("Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+								Labore reprehenderit doloribus hic itaque cupiditate unde est doloremque.")
 	end
 end
