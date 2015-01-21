9f39f37d8db1c6cbcbf2a0c6da83c283a0f83b65
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-01-21 12:24:56 +1100

    working backbone can add stuff to page with console

--- eugenius/wk09/d01/twitter_clone/app/assets/javascripts/views/StatusView.js
+++ eugenius/wk09/d01/twitter_clone/app/assets/javascripts/views/StatusView.js
@@ -1,3 +1,18 @@
 var StatusView = Backbone.View.extend({
-	
+	tagName: 'div',
+	classname: 'status',
+	template: JST['statuses/status'],
+
+	initialize: function() {
+		this.model.on("change", this.render, this);
+	},
+
+	render: function() {
+		this.$el.html(
+			this.template(
+				this.model.toJSON()
+			)
+		);
+		return this;
+	}
 });
--- eugenius/wk09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
+++ eugenius/wk09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
@@ -2,16 +2,16 @@ var TimelineView = Backbone.View.extend({
 	tagName: "div",
 	className: "timeline",
 
-	template: JST['statuses/timeline'],
-
 	initialize: function() {
 		this.collection.fetch();
 		this.collection.on("sync", this.render, this);
 	},
 
 	render: function() {
+		var self = this;
 		this.collection.each(function(status) {
 			var statusView = new StatusView({ model:status });
+			self.$el.append(statusView.render().el); // self refers to the actual timeline view
 		});
 	}     
 });
